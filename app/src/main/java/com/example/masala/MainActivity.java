0ab57ce5ee24356fb9cadfa91606f5aa05be43f7
WS-20 <masala@gmail.com>
2026-04-08 16:08:33 +0700

    Lab_02

--- app/src/main/java/com/example/masala/MainActivity.java
+++ app/src/main/java/com/example/masala/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.masala;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        numMessage = 10;
+        Log.i("numMessage = ", ""+numMessage);
+
+        numMessage++;
+        numMessage = numMessage+1;
+        Log.i("numMessage = ", ""+numMessage);
+
+        boolean isFriend = true;
+        Log.i("isFriend = ", ""+isFriend);
+        String contact = "Wanarat";
+        String message = "This is my first APP";
+
+        Toast.makeText(this, "Message from " + contact, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Message is " + message, Toast.LENGTH_SHORT).show();
+        int x = 0;
+
+        while(true){
+            x++;
+            Log.i("x = ", "" + x);
+            if(x == 3){
+                break;
+            }
+        }
+        String mess = joinThese( "Method ",  "are ",  "cool");
+        float area = getAreaCircle( 5f);
+        Log.i( "message=", mess);
+        Log.i( "area= ",  ""+area);
+
     }
+    String joinThese(String a, String b, String c){
+        return a + b + c;
+    }
+
+    float getAreaCircle(float radius){
+        return 3.14f * radius * radius;
+    }
+
+
 }
